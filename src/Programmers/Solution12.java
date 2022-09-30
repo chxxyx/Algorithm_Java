2351e5157f394aeab93cc260edca07c1dba967c7
chxxyx <codud0826@naver.com>
2022-09-30 19:58:06 +0900

    프로그래머스 알고리즘 공부 - 220930, 문자열 내 p와 y의 개수

--- src/Programmers/Solution12.java
+++ src/Programmers/Solution12.java
@@ -7,14 +7,12 @@ package Programmers;
         대문자와 소문자가 섞여있는 문자열 s가 주어집니다.
         s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요.
         'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
-
         예를 들어 s가 "pPoooyY"면 true를 return하고 "Pyy"라면 false를 return합니다.
 
         제한사항
         문자열 s의 길이 : 50 이하의 자연수
         문자열 s는 알파벳으로만 이루어져 있습니다.
  */
-
 public class Solution12 {
 
     /*
@@ -50,18 +48,18 @@ public class Solution12 {
            방법 2. 스트림, 람다 사용
            얘도 똑같음 ..
      */
-    public static boolean solution2(String s){
-
-        char p = 'p';
-        char y = 'y';
-
-        long pCount= s.chars().filter(ch -> ch == 'p').count();
-        long yCount= s.chars().filter(ch -> ch == 'y').count();
-
-
-        return pCount == yCount;
-
-    }
+//    public static boolean solution2(String s){
+//
+//      //  char p = 'p';
+//      //  char y = 'y';
+//
+//        long pCount= s.chars().filter(ch -> ch == 'p').count();
+//        long yCount= s.chars().filter(ch -> ch == 'y').count();
+//
+//
+//        return pCount == yCount;
+//
+//    }
     /*
             방법 3. toLowcase()
             찾아 보니까 문자열에 있는 대문자, 소문자 ? 때문에 정확성이 떨어지는 거 같아서
