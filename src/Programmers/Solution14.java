1081ec364429a30ba74f62a0d17f9775d6afae14
chxxyx <codud0826@naver.com>
2022-10-02 15:14:43 +0900

    프로그래머스 알고리즘 공부 -  221002, 나머지가 1이 되는 수 찾기

---
+++ src/Programmers/Solution14.java
@@ -0,0 +1,47 @@
+package Programmers;
+/*
+        221002
+
+        <나머지가 1이 되는 수 찾기>
+
+        문제 설명
+        자연수 n이 매개변수로 주어집니다.
+        n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요.
+        답이 항상 존재함은 증명될 수 있습니다.
+
+        제한사항
+        3 ≤ n ≤ 1,000,000
+
+        입출력 예
+        n	result
+        10	3
+        12	11
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution14 {
+
+
+    public static int solution(int n) {
+
+        List<Integer>result = new ArrayList<Integer>();
+
+        for (int i = 1; i < n; i++) {
+
+            if (n % i == 1) {
+               result.add(i);
+            }
+
+        }
+
+        return result.get(0);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(12));
+    }
+
+}
