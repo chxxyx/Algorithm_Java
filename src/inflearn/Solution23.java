726f9a4d5a9020d232ae7bf7a6e721013bfec0f5
chxxyx <codud0826@naver.com>
2022-11-15 21:18:04 +0900

    인프런 알고리즘 공부 -  221115, LRU

---
+++ src/inflearn/Solution23.java
@@ -0,0 +1,43 @@
+package inflearn;
+/*
+
+    221115
+    
+    <정렬, 이분검색과 결정 알고리즘>
+
+    [LRU]
+ */
+
+public class Solution23 {
+
+    public int[] solution(int size, int n, int[] arr) {
+
+        int[] cache = new int[size];
+
+        for (int x : arr) {
+
+            int pos = -1;
+            for (int i = 0; i < size; i++) {
+                if (x == cache[i]) {
+                    pos = i;
+                }
+            }
+
+            if (pos == -1) {
+                for (int i = size-1; i >= 1; i--) {
+                    cache[i] = cache[i-1];
+                }
+               // cache[0] = x; 생략 가능
+
+            } else {
+                for (int i = pos; i >= 1; i--) {
+                cache[i] = cache[i-1];
+                }
+                cache[0]=x;
+            }
+        }
+
+        return cache;
+
+    }
+}
